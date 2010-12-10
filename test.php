2f4ed4b943e0245b205063c1aca835cd5d330421
Sanković Marko <sankovic.marko@gmail.com>
2010-12-10 22:09:09 +0100

    Add test.php

--- test.php
+++ test.php
@@ -19,26 +19,4 @@
     } catch(Exception $e) {
         echo $e->getMessage();
     }
-
-/*
-    $emailLabsClient = new EmailLabs_Client('https://secure.elabs10.com/API/mailing_list.html', '2010000089', 'Wl31eRSrgI32P#');
-
-    //$result = $emailLabsClient->setMlid(68)->userExists('dan2@dan.cogentads.com');
-
-    //$result = $emailLabsClient->setMlid(68)->addData('sdflajfd', 'demographic', 2)->recordUpdate('dan2@dan.cogentads.com');
-
-    $emailLabsClient->clearData();
-
-
-
-    $result = $emailLabsClient->setMlid(68)
-                              //->addData('dan2@dan.cogentads.com', 'email')
-                              //->addData('March 17, 2010', 'extra', 'start_datetime')
-                              ->recordQueryData('dan2@dan.cogentads1.com');
-
-              var_dump($result->getData());
-
-    var_dump($result->isSuccess());
-    var_dump($result->isError());
- * 
- */
+    
