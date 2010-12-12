853e6936d73d22e36c0c02ba22469e4736ea6237
Sanković Marko <sankovic.marko@gmail.com>
2010-12-12 13:32:40 +0100

    wiki + proccess 1..3

---
+++ process_1.php
@@ -0,0 +1,25 @@
+<?php
+    // Ensure library/ is on include_path
+    set_include_path(implode(PATH_SEPARATOR, array(realpath('./library'), get_include_path())));
+
+    // Register Autoloader
+    require_once 'Zend/Loader/Autoloader.php';
+    $autoloader = Zend_Loader_Autoloader::getInstance();
+    $autoloader->registerNamespace('EmailLabs_');
+
+    // Get configurations
+    $oldSiteConfig = new Zend_Config_Ini('config.ini', 'old_site', null);
+    $newSiteConfig = new Zend_Config_Ini('config.ini', 'new_site', null);
+
+    $origin = $oldSiteConfig->toArray();
+    $target = $newSiteConfig->toArray();
+
+    $emailLabsSync = new EmailLabs_Sync($origin, $target);
+    $logger = new EmailLabs_Logger('log/sync.log');
+
+    try {
+        $emailLabsSync->attach();
+        $emailLabsSync->syncRecords(array('type' => 'active', 'start_datetime' => 300));
+    } catch(Exception $e) {
+        echo $e->getMessage();
+    }
